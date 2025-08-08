5b7d77c6e95cb9f42d8957570deb3e1a081dcdb6
sho96 <shotaro962008@gmail.com>
2025-08-09 06:37:03 +0900

    asdf

--- app/api/cron/route.js
+++ app/api/cron/route.js
@@ -1,30 +1,69 @@
-import { addExpiredItem, addExpiredLeftover, getAllItemsFormatted, getLeftoversFormatted, incrementNumberOfExpirations } from "@/utils/database";
+import {
+  addExpiredItem,
+  addExpiredLeftover,
+  getAllItemsFormatted,
+  getLeftoversFormatted,
+  incrementNumberOfExpirations,
+} from "@/utils/database";
 import { NextResponse } from "next/server";
-export async function GET(){
+export async function GET() {
   const allItems = await getAllItemsFormatted();
   const allLeftovers = await getLeftoversFormatted();
 
-  const expiredItems = allItems.filter(item => item.expirationDate < new Date());
-  const expiredLeftovers = allLeftovers.filter(leftover => leftover.expirationDate < new Date());
+  const previousDate = new Date()
+  previousDate.setHours(previousDate.getHours() + 9, 0, 0, 0);
+  previousDate.setHours(0, 0, 0, 0);
+  console.log(previousDate);
 
-  const newlyExpiredItems = (await Promise.all(
-    expiredItems.map(async item => {
-      return await addExpiredItem(item.id);
-    })
-  )).filter(leftover => leftover);
+  const expiredItems = allItems.filter(
+    (item) => item.expirationDate < previousDate
+  );
+  const expiredLeftovers = allLeftovers.filter(
+    (leftover) => leftover.expirationDate < previousDate
+  );
 
-  const newlyExpiredLeftovers = (await Promise.all(
-    expiredLeftovers.map(async leftover => {
-      return await addExpiredLeftover(leftover.id);
-    })
-  )).filter(leftover => leftover);
+  const newlyExpiredItems = (
+    await Promise.all(
+      expiredItems.map(async (item) => {
+        const notAdded = await addExpiredItem(item.id);
+        if (notAdded) {
+          return {
+            id: leftover.id,
+            name: leftover.name,
+          };
+        }
+        return null;
+      })
+    )
+  ).filter((leftover) => leftover);
+
+  const newlyExpiredLeftovers = (
+    await Promise.all(
+      expiredLeftovers.map(async (leftover) => {
+        const notAdded = await addExpiredLeftover(leftover.id);
+        if (notAdded) {
+          return {
+            id: leftover.id,
+            name: leftover.name,
+          };
+        }
+        return null;
+      })
+    )
+  ).filter((leftover) => leftover);
 
   const totalExpired = newlyExpiredItems.length + newlyExpiredLeftovers.length;
-  
-  incrementNumberOfExpirations(new Date().toISOString().split('T')[0], totalExpired);
-  
-  console.log("Expired items: ", newlyExpiredItems);
-  console.log("Expired leftovers: ", newlyExpiredLeftovers);
+
+  incrementNumberOfExpirations(
+    new Date().toISOString().split("T")[0],
+    totalExpired
+  );
+
+  console.log("Expired items: ", expiredItems);
+  console.log("Expired leftovers: ", expiredLeftovers);
+  console.log("Newly expired items: ", newlyExpiredItems);
+  console.log("Newly expired leftovers: ", newlyExpiredLeftovers);
+  console.log("Total expired: ", totalExpired);
 
   return NextResponse.json({ success: true });
 }
